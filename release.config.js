9a1d72d4dee9a7bcb07e4390b521f9497bbba16b
Bulat Shakirzyanov <83289+avalanche123@users.noreply.github.com>
2020-07-20 16:46:06 -0400

    update release branches patterns

--- release.config.js
+++ release.config.js
@@ -20,4 +20,12 @@ module.exports = {
     ],
     '@semantic-release/github',
   ],
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    {name: 'beta', prerelease: true},
+    {name: 'alpha', prerelease: true},
+  ],
 };
