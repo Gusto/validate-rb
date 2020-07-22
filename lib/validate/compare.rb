052c06423c3ceac94f57bedcb2fa222bef9c3526
Bulat Shakirzyanov <83289+avalanche123@users.noreply.github.com>
2020-07-21 21:20:22 -0400

    improve compare dsl

--- lib/validate/compare.rb
+++ lib/validate/compare.rb
@@ -2,34 +2,7 @@
 
 module Validate
   module Compare
-    class WithAttributes
-      include Comparable
-      prepend UsingTransformation
-
-      def initialize(attributes)
-        @attributes = attributes
-      end
-
-      def <=>(other)
-        @attributes.map { |attribute, value| value <=> other.send(attribute) }
-                   .find { |result| !result.zero? } || 0
-      end
-    end
-
-    class ToValue
-      include Comparable
-      prepend UsingTransformation
-
-      def initialize(value_block)
-        @value_block = value_block
-      end
-
-      def <=>(other)
-        @value_block.call <=> other
-      end
-    end
-
-    module UsingTransformation
+    module TransformUsing
       def using(&transform_block)
         @transform_block = transform_block
         self
@@ -39,6 +12,56 @@ module Validate
         return super if @transform_block.nil?
 
         super(@transform_block.call(other))
+      end
+    end
+
+    class WithAttributes
+      include Comparable
+      prepend TransformUsing
+
+      def initialize(attributes)
+        @attributes = attributes
+      end
+
+      def <=>(other)
+        @attributes.each do |attribute, value|
+          result = value <=> other.send(attribute)
+          return result unless result.zero?
+        end
+
+        0
+      end
+
+      def method_missing(symbol, *args)
+        return super unless args.empty? && respond_to_missing?(symbol)
+
+        @attributes[symbol]
+      end
+
+      def respond_to_missing?(attribute, _ = false)
+        @attributes.include?(attribute)
+      end
+
+      def to_s
+        '<attributes ' + @attributes.map { |attribute, value| "#{attribute}: #{value}"}
+                                    .join(', ') + '>'
+      end
+    end
+
+    class ToValue
+      include Comparable
+      prepend TransformUsing
+
+      def initialize(value_block)
+        @value_block = value_block
+      end
+
+      def <=>(other)
+        @value_block.call <=> other
+      end
+
+      def to_s
+        '<dynamic value>'
       end
     end
 
